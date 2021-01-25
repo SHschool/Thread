313999c05053ad69d9bdc6f12788b094aca40f6c
tomo020120 <73629880+tomo020120@users.noreply.github.com>
2021-01-25 15:51:30 +0900

    Update and rename userInfoRegist.java to userInfoRegistServlet.java
    ユーザー情報登録用サーブレット

---
+++ WEB-INF/classes/userInfoRegistServlet.java
@@ -0,0 +1,31 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException; 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserInfoRegistServlet extends HttpServlet{
+    //ArrayList使用
+    private ArrayList<  > userInfo = new ArrayList<  >();
+
+    public void doPost(HttpServletRequest req,HttpServletResponse res) 
+    throws IOException,ServletException{
+        //エンコード設定
+        req.setCharacterEncoding("Windows-31J");
+
+        //POST要求の値を取得
+        int id = req.getParameter("id");
+        String name = req.getParameter("name");
+
+        //Insertクラスをインスタンス化して値を渡す。
+
+        //値をJSPに渡すため登録
+
+        RequestDispatcher dis = req.getRequestDispatcher("sendPost"); //掲示板スレッド画面へ情報を送る
+        dis.forward(req,res);
+    }
+}
