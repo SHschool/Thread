645c3fa480e327139aaf76a90cdc1808e07bc2a1
SHschool <73629640+SHschool@users.noreply.github.com>
2021-02-01 10:18:45 +0900

    Update and rename insertThread.java to ThreadDataBase.java
    name is changed

--- WEB-INF/classes/orcl/ThreadDataBase.java
+++ WEB-INF/classes/orcl/ThreadDataBase.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 package orcl;
 
-public class ThreadDB{
+public class ThreadDataBase{
 
 	// public static void main(String[] args){
 	private boolean insertFlag = false;
