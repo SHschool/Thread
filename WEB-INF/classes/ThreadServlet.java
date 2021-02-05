82f37a1bec1cf9476a2e307042baccabda5006fc
tomo020120 <73629880+tomo020120@users.noreply.github.com>
2021-02-05 15:21:00 +0900

    Update ThreadServlet.java

--- WEB-INF/classes/ThreadServlet.java
+++ WEB-INF/classes/ThreadServlet.java
@@ -13,54 +13,50 @@ import bean.TB_POST_Bean;
 public class ThreadServlet extends HttpServlet {
     private ArrayList<TB_POST_Bean> threads = new ArrayList<TB_POST_Bean>();
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse res)
-             throws ServletException, IOException {
-                threads.clear();
-                req.setCharacterEncoding("Windows-31J");
-                
-                ThreadDataBase th_db = new ThreadDataBase();
-                ArrayList<TB_POST_Bean> th_data = th_db.SelectThreadInfo();
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        threads.clear();
+        req.setCharacterEncoding("Windows-31J");
 
-                req.setAttribute("threads",th_data);
-                
-                RequestDispatcher dis = req.getRequestDispatcher("index");
-                dis.forward(req,res);
-             }
+        ThreadDataBase th_db = new ThreadDataBase();
+        ArrayList<TB_POST_Bean> th_data = th_db.SelectThreadInfo();
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
+        req.setAttribute("threads", th_data);
+
+        RequestDispatcher dis = req.getRequestDispatcher("index.jsp");
+        dis.forward(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         // クライアントからのrequestに含まれていたデータの
         // 文字コードを指定する
         req.setCharacterEncoding("Windows-31J");
+        threads.clear();
 
-        //POST要求によって送信されたパラメータを取得する
+        // POST要求によって送信されたパラメータを取得する
         String name = req.getParameter("name");
         String content = req.getParameter("content");
         String tag = req.getParameter("tag");
 
-        //oracle接続クラスをよびデータを挿入する
+        // oracle接続クラスをよびデータを挿入する
         ThreadDataBase th_db = new ThreadDataBase();
-        
-        if(th_db.IsThreadInsert(name,content,tag)){
-            TB_POST_Bean threadInfo = new TB_POST_Bean();
-            threadInfo.setUser_name(name);
-            threadInfo.setContent(content);
-            threadInfo.setTag(tag);
 
-            threads.add(threadInfo); //ArrayListにBeanのインスタンスを渡す
+        if (th_db.IsThreadInsert(name, content, tag)) {
+            ArrayList<TB_POST_Bean> th_data = th_db.SelectThreadInfo();
+
+            for(int i = 0; i < th_data.size(); i++){
+                TB_POST_Bean info = (TB_POST_Bean)th_data.get(i);
+                threads.add(info);
+            }
         }
 
-
-
         // 式言語として登録
-       req.setAttribute("threads",threads);
+        req.setAttribute("threads", threads);
 
         // RequestDispatcherインターフェイスを実装するクラスのインスタンスを取得する
         // 引数は転送先のURL
-        RequestDispatcher dispatcher =
-                req.getRequestDispatcher("index");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
 
-        //転送先に要求を転送する
+        // 転送先に要求を転送する
         dispatcher.forward(req, res);
     }
 }
