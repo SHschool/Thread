c7c3211509b36a718412e1abecf5a921a737b762
tomo020120 <73629880+tomo020120@users.noreply.github.com>
2021-02-08 09:39:45 +0900

    Update ThreadServlet.java

--- WEB-INF/classes/ThreadServlet.java
+++ WEB-INF/classes/ThreadServlet.java
@@ -11,22 +11,30 @@ import orcl.ThreadDataBase;
 import bean.TB_POST_Bean;
 
 public class ThreadServlet extends HttpServlet {
-    private ArrayList<TB_POST_Bean> threads = new ArrayList<TB_POST_Bean>();
+    //beanのインスタンスを受け取るArrayList
+    private ArrayList<TB_POST_Bean> threads = new ArrayList<TB_POST_Bean>(); 
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        threads.clear();
-        req.setCharacterEncoding("Windows-31J");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) 
+    throws ServletException, IOException { //URLをたたいた時スレッドの一覧を表示するメソッド
 
-        ThreadDataBase th_db = new ThreadDataBase();
+        req.setCharacterEncoding("Windows-31J"); //エンコード指定
+        threads.clear(); //一度中身を空にする
+
+        //データベースに接続してinsertなどするメソッドがあるクラスをインスタンス化
+        ThreadDataBase th_db = new ThreadDataBase(); 
+
+         //データベースの投稿記事データを取得(戻り値：beanのインスタンスを格納したArrayList)
         ArrayList<TB_POST_Bean> th_data = th_db.SelectThreadInfo();
 
-        req.setAttribute("threads", th_data);
+        req.setAttribute("threads", th_data); //JSPで使えるよう登録
 
-        RequestDispatcher dis = req.getRequestDispatcher("index.jsp");
-        dis.forward(req, res);
+        RequestDispatcher dis = req.getRequestDispatcher("index.jsp"); //転送先指定
+        dis.forward(req, res); //転送
     }
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) 
+    throws ServletException, IOException { //新規作成のデータを登録して表示
+
         // クライアントからのrequestに含まれていたデータの
         // 文字コードを指定する
         req.setCharacterEncoding("Windows-31J");
@@ -40,23 +48,8 @@ public class ThreadServlet extends HttpServlet {
         // oracle接続クラスをよびデータを挿入する
         ThreadDataBase th_db = new ThreadDataBase();
 
-        if (th_db.IsThreadInsert(name, content, tag)) {
-            ArrayList<TB_POST_Bean> th_data = th_db.SelectThreadInfo();
-
-            for(int i = 0; i < th_data.size(); i++){
-                TB_POST_Bean info = (TB_POST_Bean)th_data.get(i);
-                threads.add(info);
+        if (th_db.IsThreadInsert(name, content, tag)) { //データベースに投稿出来たらdoGetメソッドを呼んで再度一覧表示
+                doGet(req,res);
             }
         }
-
-        // 式言語として登録
-        req.setAttribute("threads", threads);
-
-        // RequestDispatcherインターフェイスを実装するクラスのインスタンスを取得する
-        // 引数は転送先のURL
-        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
-
-        // 転送先に要求を転送する
-        dispatcher.forward(req, res);
     }
-}
